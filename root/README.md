9f12ab8965fa3d92fcc09a006d93853bcbf07105
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2014-03-27 18:58:15 +0400

    feature: initial commit

---
+++ root/README.md
@@ -0,0 +1,4 @@
+    ./{%= name %}_manager up
+    ./{%= name %}_manager down
+    ./{%= name %}_manager deploy
+    ./{%= name %}_manager cleanup
---
+++ template.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+
+module.exports = {
+  warnOn: '*',
+
+  template: function(grunt, init, callback) {
+    init.process({}, [
+      init.prompt('name'),
+      init.prompt('packages', ''),
+      init.prompt('github_username', 'vslinko'),
+      init.prompt('github_repo', 'vslinko/grunt-init-project'),
+      init.prompt('build_system', 'gulp'),
+      init.prompt('build_dir', ''),
+      init.prompt('ip_address', '10.10.10.10')
+    ], function(err, props) {
+      props['packages_json_array'] = JSON.stringify(props.packages.split(/\s+/));
+      init.copyAndProcess(init.filesToCopy(props), props);
+      fs.chmodSync(props.name + '_manager', 0755);
+      callback();
+    });
+  }
+};
